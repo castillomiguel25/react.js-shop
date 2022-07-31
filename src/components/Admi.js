e988f5e1abbef3cd77a4d0e96d7c4323afb852d9
castillomiguel25 <miguelangelcastillo947@gmail.com>
2022-07-31 12:12:13 -0400

    'status'

--- src/components/Admi.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Admi = () => {
-  return (
-    <div>Hola has iniciado sesión como administrador</div>
-  )
-}
-
-export default Admi
--- src/components/Users.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Users = () => {
-  return (
-    <div>Hola has iniciado sesión como administrador</div>
-  )
-}
-
-export default Users
